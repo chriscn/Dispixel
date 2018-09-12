8103727ba317588ac115d47a083d42e5289e65b1
DanceDog <dancedog1203@gmail.com>
2018-09-12 19:19:52 -0400

    Began command system

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const config = require('config.json');
+const config = require('./config.json'),
+commands = require('./lib/commands.js');
+
+//Discord setup
+const Discord = require('discord.js'),
+client = new Discord.Client();
+client.login(config.token);
+
+//Get everything running
+commands.listen(client);
