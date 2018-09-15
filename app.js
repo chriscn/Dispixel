fce5cbab5df99df2d565bb6953d65566ffd7841a
Christopher Nethercott <ccnethercott@gmail.com>
2018-09-15 20:05:44 +0100

    Updated app.js to pull value from config.json

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ commands = require('./libs/commands.js');
 //Discord setup
 const Discord = require('discord.js'),
 bot = new Discord.bot();
-bot.login(config.token);
+bot.login(config.discord_token);
 
 bot.on('ready' () => {
   console.log("Dispixel is ready to rock and roll!");
