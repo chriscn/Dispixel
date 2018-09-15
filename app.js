d5daae977501d0b366341a6c7db0d8fe31c60a34
Christopher Nethercott <ccnethercott@gmail.com>
2018-09-15 20:13:15 +0100

    Updated with a comma

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const Discord = require('discord.js'),
 bot = new Discord.bot();
 bot.login(config.discord_token);
 
-bot.on('ready' () => {
+bot.on('ready', () => {
   console.log("Dispixel is ready to rock and roll!");
 });
 
