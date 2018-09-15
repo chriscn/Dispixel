1f8325557c5168739b9cbd61fe3ebe264455d8c3
Dance-Dog <bnagle1203@icloud.com>
2018-09-15 15:44:10 -0400

    Added a missing comma + shortened to 1 line

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const Discord = require('discord.js'),
 bot = new Discord.bot();
 bot.login(config.discord_token);
 
-bot.on('ready' () => {
-  console.log("Dispixel is ready to rock and roll!");
-});
+bot.on('ready', () => console.log("Dispixel is ready to rock and roll!"));
 
 //Get everything running
 commands.listen(bot);
