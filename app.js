f12329e9565bb95a34ff7a6c1418bed5e1514ea5
Dance-Dog <bnagle1203@icloud.com>
2018-09-21 16:32:26 -0400

    Removed logic that shouldn't be there

--- app.js
+++ app.js
@@ -1,51 +1,14 @@
 const token = require('./key.json').discord_token;
 const commands = require('./libs/commands.js');
 const config = require('./config.json');
-const prefix = config.prefix;
 const moment = require('moment');
 
 // Discord setup
-const Discord = require('discord.js');
-
-
-const bot = new Discord.Client();
+const Discord = require('discord.js'),
+bot = new Discord.Client();
 bot.login(token);
 
-bot.on('ready', () => {
-  console.log(`Dispixel is ready to rock and roll! Started at ${moment().format()}`);
-});
+bot.on('ready', () => console.log(`Dispixel is ready to rock and roll! Started at ${moment().format()}`));
 
-bot.on('message', (message) => {
-  if (!message.content.startsWith(prefix) || message.author.bot) return; // does nothing if the message doesn't start with the prefix or user is a bot
-
-  const args = message.content.slice(prefix.length).split(/ +/);
-
-
-  const command = args.shift().toLowerCase();
-
-
-  const sender = message.author;
-
-  if (command === 'ping') {
-    message.channel.send(`Pong! ${bot.ping}ms`);
-  }
-});
-
-// Logging
-bot.on('message', (message) => {
-  if (!message.content.startsWith(config.prefix) || message.author.bot) return; // does nothing if the message doesn't start with the prefix or user is a bot
-
-  const args = message.content.slice(config.prefix.length).split(/ +/);
-
-
-  const command = args.shift().toLowerCase();
-
-
-  const sender = message.author.username;
-
-  console.log(`[COMMAND] <${command}> ${!args.length ? '':` with arguments: ${args.join(', ')}; `}from ${sender}`);
-});
-
-
-// Get everything running
+//Listen for commands
 commands.listen(bot);
