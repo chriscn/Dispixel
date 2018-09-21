71ba66420987afaf6b7beebe8ef5a5b0c880b5b3
Dance-Dog <bnagle1203@icloud.com>
2018-09-21 18:55:42 -0400

    Fixed the player command to work with the latest version of hypixeljs

--- libs/commands.js
+++ libs/commands.js
@@ -40,13 +40,13 @@ function listen(bot) {
       });
       function sendPlayerEmbed(player) {
         const playerEmbed = new Discord.RichEmbed()
-        .setTitle(player.displayname + '\'s Statistics')
+        .setTitle(`${player.displayname} **(Currently ${util.isOnline(player.lastLogin, player.lastLogout)})**`)
         .setThumbnail('https://visage.surgeplay.com/face/' + player.uuid)
-        .addField('Rank:', `${player.displayRank ? `**[${player.displayRank}]** *(Actually ${player.baseRank})*` : `**[${player.baseRank}]**`}`, true)
-        .addField('Level:', player.level, true)
-        .addField('Karma:', player.karma ? util.numberWithCommas(player.karma) : 0)
-        .addField('Achievement Points:', player.achievementPoints ? util.numberWithCommas(player.achievementPoints) : 0)
-        .addField('Joined: ', (! player.firstJoined ? util.formatAPITime(player.firstJoined) : `Hasn't Joined`));
+        .addField('Rank:', util.getRank(player), true)
+        .addField('Level:', util.networkLevel(player.networkExp), true)
+        .addField('Karma:', player.karma ? util.numberWithCommas(player.karma) : 0, true)
+        .addField('Achievement Points:', player.achievementPoints ? util.numberWithCommas(player.achievementPoints) : 0, true)
+        .addField('Joined: ', ! player.firstJoined ? util.formatAPITime(player.firstJoined) : `Hasn't Joined`, true);
         message.channel.send(playerEmbed);
       }
     });
