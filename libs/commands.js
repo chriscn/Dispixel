0609a0dd7ec907ce2524ea3a6e2c06debef982e1
Dance-Dog <bnagle1203@icloud.com>
2018-09-21 16:16:47 -0400

    Fixed some weird variable declarations, spacing and callback hell

--- libs/commands.js
+++ libs/commands.js
@@ -1,37 +1,21 @@
 const config = require('../config.json');
-
-
 const hypixel = require('hypixeljs');
-
 // images = require('./images/images.js'),
-
 const util = require('./util.js');
-
-
 const Discord = require('discord.js');
 
 module.exports = {
   listen: (bot) => bot.on('message', (message) => {
     if (!(new RegExp(`^${config.prefix}[A-Za-z0-9]+(.*)$`).test(message.content))) return;
 
-    const args = message.content.replace(config.prefix, '').trim().split(/ +/g);
+    const args = message.content.replace(config.prefix, '').trim().split(/ +/g),
+    cmd = args.shift().toLowerCase();
 
-
-    const cmd = args.shift().toLowerCase();
-
-    const channel = message.channel;
-
-
-    const content = message.cleanContent;
-
-
-    const author = message.author;
-
-
-    const member = message.member;
-
-
-    const guild = message.guild;
+    const channel = message.channel,
+    content = message.cleanContent,
+    author = message.author,
+    member = message.member,
+    guild = message.guild;
 
     function error(err) {
       channel.send(util.errorEmbed(err)).catch((err) => console.log(err));
@@ -41,33 +25,19 @@ module.exports = {
       if (err) return error(err);
       hypixel.getPlayerByName(args[0], (err, player) => {
         if (err) return error(err);
-        /* images.playerCard(player, (err, path) => {
-          var statsEmbed = new Discord.RichEmbed()
-          .setTitle(`${player.displayname}'s General Stats`)
-          .setColor(0x33cc33)
-          //.setImage(`attachment://image.png`)
-          .setThumbnail(`https://visage.surgeplay.com/head/${player.uuid}`);
-
-          //Stats
-          statsEmbed
+        sendPlayerEmbed();
+      });
+      function sendPlayerEmbed() {
+        const playerEmbed = new Discord.RichEmbed()
+          .setTitle(player.displayname + '\'s Statistics')
+          .setThumbnail('https://visage.surgeplay.com/face/' + player.uuid)
           .addField('Rank:', `${player.displayRank ? `**[${player.displayRank}]** *(Actually ${player.baseRank})*` : `**[${player.baseRank}]**`}`, true)
           .addField('Level:', player.level, true)
           .addField('Karma:', player.karma ? util.numberWithCommas(player.karma) : 0)
           .addField('Achievement Points:', player.achievementPoints ? util.numberWithCommas(player.achievementPoints) : 0)
-          .addField('Joined: ', (!  player.firstJoined ? util.formatAPITime(player.firstJoined) : `Hasn't Joined`));
-
-          channel.send(statsEmbed);
-        });*/
-        const playerEmbed = new Discord.RichEmbed()
-            .setTitle(player.displayname + '\'s Statistics')
-            .setThumbnail('https://visage.surgeplay.com/face/' + player.uuid)
-            .addField('Rank:', `${player.displayRank ? `**[${player.displayRank}]** *(Actually ${player.baseRank})*` : `**[${player.baseRank}]**`}`, true)
-            .addField('Level:', player.level, true)
-            .addField('Karma:', player.karma ? util.numberWithCommas(player.karma) : 0)
-            .addField('Achievement Points:', player.achievementPoints ? util.numberWithCommas(player.achievementPoints) : 0)
-            .addField('Joined: ', (! player.firstJoined ? util.formatAPITime(player.firstJoined) : `Hasn't Joined`));
+          .addField('Joined: ', (! player.firstJoined ? util.formatAPITime(player.firstJoined) : `Hasn't Joined`));
         message.channel.send(playerEmbed);
-      });
+      }
     });
   }),
 };
