5efa25114c689ae438347f211bebb99137d31d69
Dance-Dog <bnagle1203@icloud.com>
2018-09-21 16:44:18 -0400

    Now logs in with API keys

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const token = require('./key.json').discord_token;
+const access = require('./key.json');
 const commands = require('./libs/commands.js');
 const config = require('./config.json');
 const moment = require('moment');
 
-// Discord setup
+//Discord setup
 const Discord = require('discord.js'),
 bot = new Discord.Client();
-bot.login(token);
+bot.login(access.discord_token);
+
+//Hypixel API setup
+const hypixel = require('hypixeljs');
+hypixel.login(access.hypixel_api_keys);
 
 bot.on('ready', () => console.log(`Dispixel is ready to rock and roll! Started at ${moment().format()}`));
 
