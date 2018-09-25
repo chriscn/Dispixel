337b93d798d0c7c28ebc934d016d29fb3a7c5dc0
Christopher Nethercott <ccnethercott@gmail.com>
2018-09-25 17:37:53 +0100

    Not sure why this doesn't work gonna work on a new command

--- app.js
+++ app.js
@@ -14,9 +14,11 @@ hypixel.login(access.hypixel_api_keys);
 
 bot.on('ready', () => console.log(`Dispixel is ready to rock and roll! Started at ${moment().format()}`));
 
-bot.on('message', (message) => {
-  message.channels.get(config.logging_channel).send(`[COMMAND] \'-${cmd} ${args.join(' ')}\' was recieved from \'${author.username}\' in channel \'${channel.id}\'`);
-});
+/*bot.on('message', (message) => {
+  const args = message.content.replace(config.prefix, '').trim().split(/ +/g);
+  const cmd = args.shift().toLowerCase()
+  message.channel.get(config.logging_channel).send(`[COMMAND REQUEST] \'-${cmd} ${args.join(' ')}\' was recieved from ${message.author}`)
+});*/
 
 //Listen for commands
 commands.listen(bot);
