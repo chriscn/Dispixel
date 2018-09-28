07a579d21ed63cbfadafc36c71cd216a40777c7c
Christopher Nethercott <ccnethercott@gmail.com>
2018-09-28 17:08:12 +0100

    Updated logging feature we debug

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const access = require('./key.json');
 const commands = require('./libs/commands.js');
 const config = require('./config.json');
 const moment = require('moment');
+const debug = true;
 
 //Discord setup
 const Discord = require('discord.js'),
@@ -17,14 +18,12 @@ bot.on('ready', () => console.log(`Dispixel is ready to rock and roll! Started a
 bot.on('message', (message) => {
   const args = message.content.replace(config.prefix, '').trim().split(/ +/g);
   const cmd = args.shift();
-  /*message.channel.get(config.logging_channel).send*/console.log(`[COMMAND REQUEST] by ${message.username} requesting ${cmd} with arguments ${args}`);
+  if (debug) {
+    console.log(`[COMMAND REQUEST] by ${message.username} requesting ${cmd} with arguments ${args}`);
+  } else {
+    message.get(config.logging_channel).send`[COMMAND REQUEST] by ${message.username} requesting ${cmd} with arguments ${args}`);
+  }
 });
-
-/*bot.on('message', (message) => {
-  const args = message.content.replace(config.prefix, '').trim().split(/ +/g);
-  const cmd = args.shift().toLowerCase()
-  message.channel.get(config.logging_channel).send(`[COMMAND REQUEST] \'-${cmd} ${args.join(' ')}\' was recieved from ${message.author}`)
-});*/
 
 //Listen for commands
 commands.listen(bot);
