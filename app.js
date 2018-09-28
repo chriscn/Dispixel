25c53584a449c40e835510e0aba82dd675800896
Christopher Nethercott <ccnethercott@gmail.com>
2018-09-28 17:23:10 +0100

    We now have logging to a dedicated channel. Fixes #2

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const access = require('./key.json');
-const commands = require('./libs/commands.js');
-const config = require('./config.json');
+const access = require(__dirname +'/key.json');
+const commands = require(__dirname + '/libs/commands.js');
+const config = require(__dirname + '/config.json');
 const moment = require('moment');
-const debug = true;
+const debug = false;
 
 //Discord setup
 const Discord = require('discord.js'),
@@ -13,17 +13,23 @@ bot.login(access.discord_token);
 const hypixel = require('hypixeljs');
 hypixel.login(access.hypixel_api_keys);
 
-bot.on('ready', () => console.log(`Dispixel is ready to rock and roll! Started at ${moment().format()}`));
+bot.on('ready', () => {
+  bot.channels.get(config.logging_channel).send(`Dispixel is ready to rock and roll! Started at ${moment().format()}`);
+});
+
 
 bot.on('message', (message) => {
-  const args = message.content.replace(config.prefix, '').trim().split(/ +/g);
+  if (message.author.bot) return;
+  const args = message.content.replace(config.prefix, '').split(/ +/g); // splits the message with each space
   const cmd = args.shift();
+  const user = message.member.user.id;
   if (debug) {
-    console.log(`[COMMAND REQUEST] by ${message.username} requesting ${cmd} with arguments ${args}`);
+    console.log(`[COMMAND REQUEST] by ${user} (${message.author.username}) requesting ${cmd} with arguments: ${args}`);
   } else {
-    message.get(config.logging_channel).send`[COMMAND REQUEST] by ${message.username} requesting ${cmd} with arguments ${args}`);
+    bot.channels.get(config.logging_channel).send(`[COMMAND REQUEST] by ${user} (${message.author.username}) requesting ${cmd} with arguments ${args}`);
   }
 });
 
 //Listen for commands
 commands.listen(bot);
+
