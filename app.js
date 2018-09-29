447327ef3593d0370bc33e68c04598b6b39d6ecd
Christopher Nethercott <ccnethercott@gmail.com>
2018-09-29 11:16:31 +0100

    other.js :P

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const access = require('./key.json');
 const commands = require('./lib/commands.js');
 const config = require('./config.json');
 const moment = require('moment');
-const cmd_other = require('./lib/commands/other.js');
 const Discord = require('discord.js');
 const hypixel = require('hypixeljs');
+
+const cmd_other = require('./lib/command/other.js');
+const cmd_guild = require('./lib/command/guild.js');
+const cmd_player = require('./lib/command/player.js');
 
 // Discord setup
 const bot = new Discord.Client();
@@ -14,7 +17,7 @@ bot.on('ready', () => console.log(`Dispixel is ready to rock and roll! Started a
 // Hypixel API setup
 hypixel.login(access.hypixel_api_keys);
 
-// Logging Feature of all our commands.
+// Logging Feature of all our command.
 bot.on('message', (message) => {
 	if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 	const args = message.content.replace(config.prefix, '').split(/ +/g); // splits the message with each space
@@ -23,7 +26,7 @@ bot.on('message', (message) => {
 	console.log(`[COMMAND REQUEST] by ${user} (${message.author.username}) requesting ${cmd} with arguments: ${args} from ${message.guildID == null ? 'Direct Messages' : message.guildID}`);
 });
 
-// Listen for commands
+// Listen for command
 commands.listen(bot);
 cmd_other.listen(bot);
 
