4c10ccd04f680c89d049c5e699c1bbf0175aa2b0
Christopher Nethercott <ccnethercott@gmail.com>
2018-09-29 10:56:26 +0100

    This is one of the complete rewrites before sunday's release

--- libs/commands.js
+++ libs/commands.js
@@ -4,6 +4,25 @@ const hypixel = require('hypixeljs');
 const util = require('./util.js');
 const Discord = require('discord.js');
 
+module.exports = {
+	listen: (bot) => {
+		bot.on('message', (message) => {
+			if (message.content.startsWith(config.prefix) || message.author.bot) return; // making sure that we are doing our commands.
+
+			const args = message.content.replace(config.prefix, '').trim().split(/ +/g);
+			const cmd = args.shift().toLocaleLowerCase();
+
+			util.isCommand(cmd, args, 'ping', (err) => {
+				if (err) {
+					console.error(err);
+				}
+
+			});
+
+		});
+	}
+};
+
 function listen(bot) {
 	bot.on('message', (message) => {
 		if (!(new RegExp(`^${config.prefix}[A-Za-z0-9]+(.*)$`).test(message.content)) || message.author.bot) return;
