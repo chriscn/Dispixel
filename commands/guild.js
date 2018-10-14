c1acaf4b05464a998cc973a0ad24beb2e0b5eeb1
Christopher Nethercott <ccnethercott@gmail.com>
2018-10-14 22:49:01 +0100

    Adding back the guild master command

--- commands/guild.js
+++ commands/guild.js
@@ -7,7 +7,7 @@ function getGuildEmbed(guild) {
 		.setTitle(`${guild.name}`)
 		.setURL(`https://hypixel.net/guilds/${guild._id}`)
 		.setThumbnail(guild.banner ? `https://hypixel.net/data/guild_banners/100x200/${guild._id}.png` : 'https://hypixel.net/styles/hypixel-uix/hypixel/default-guild-banner.png')
-		// .addField('Guild Master:', guild.master, true)
+		.addField('Guild Master:', guild.master, true)
 		.addField('Members:', guild.members.length, true)
 		.addField('Level:', util.guildLevel(guild.exp), true)
 		.addField('Experience:', util.numberWithCommas(guild.exp), true)
@@ -24,13 +24,29 @@ module.exports = {
 			hypixeljs.getGuild.byId(args[0], (err, guild) => {
 				if (err) console.error(err);
 				if (guild == null) message.reply('that guild doesn\'t exist. Perhaps you should create it?');
-				message.channel.send(getGuildEmbed(guild));
+				guild.members.forEach(member => {
+					if (['GUILDMASTER', 'Guild Master'].indexOf(member.rank) + 1) {
+						hypixeljs.getPlayer.byUuid(member.uuid, (err, player) => {
+							if (err) return console.error(err);
+							guild.master = player.displayname;
+							message.channel.send(getGuildEmbed(guild));
+						});
+					}
+				});
 			});
 		} else {
 			hypixeljs.getGuild.byName(args.join(' '), (err, guild) => {
 				if (err) console.error(err);
-				if (guild == null) return message.reply('that guild doesn\'t exist. Perhaps you should create it?');
-				message.channel.send(getGuildEmbed(guild));
+				if (guild == null) message.reply('that guild doesn\'t exist. Perhaps you should create it?');
+				guild.members.forEach(member => {
+					if (['GUILDMASTER', 'Guild Master'].indexOf(member.rank) + 1) {
+						hypixeljs.getPlayer.byUuid(member.uuid, (err, player) => {
+							if (err) return console.error(err);
+							guild.master = player.displayname;
+							message.channel.send(getGuildEmbed(guild));
+						});
+					}
+				});
 			});
 		}
 	},
