952ad90b3eb7a9e952f84cf13c35114a3c4c104c
Christopher Nethercott <ccnethercott@gmail.com>
2018-10-21 22:59:01 +0100

    pushing for enginner man

--- commands/guild.js
+++ commands/guild.js
@@ -3,19 +3,34 @@ const hypixeljs = require('hypixeljs');
 const util = require('../methods');
 
 function getGuildEmbed(guild) {
-	guild.members.forEach(member => {
-		if (member.rank === 'Guild Master') {
-			hypixeljs.getPlayer.byUuid(member.uuid, (err, player) => {
-				guild.master = player.displayname;
-			});
+	let guildMaster = '';
+
+	for (let i = 0; i < guild.members.length; i++) {
+		if (guild.members[i] != null) {
+			console.log(`On member ${guild.members[i].uuid} which is number ${i}`);
+			if (['GUILDMASTER', 'Guild Master'].indexOf(guild.members[i].rank) >= 0) {
+				console.log(`The player ${guild.members[i].uuid} is the guild master.`);
+				hypixeljs.getPlayer.byUuid(guild.members[i].uuid, (err, player) => {
+					if (err) console.error(err);
+					console.log(`The guild masters username is ${player.displayname}`);
+					guildMaster = player.displayname;
+				});
+			}
 		}
-	});
+	}
+
+	//{ uuid: '365678b8c0944feba58ed3b1488d106b',
+	//   rank: 'Guild Master',
+	//   joined: 1455213421874,
+	//   questParticipation: 4 }
+
+	if (guildMaster === '' || guildMaster === undefined || guildMaster == null) guildMaster = 'You fucked up.';
 
 	return new Discord.RichEmbed()
 		.setTitle(`${guild.name}`)
 		.setURL(`https://hypixel.net/guilds/${guild._id}`)
 		.setThumbnail(guild.banner ? `https://hypixel.net/data/guild_banners/100x200/${guild._id}.png` : 'https://hypixel.net/styles/hypixel-uix/hypixel/default-guild-banner.png')
-		.addField('Guild Master:', guild.master, true)
+		.addField('Guild Master:', guildMaster, true)
 		.addField('Members:', guild.members.length, true)
 		.addField('Level:', util.guildLevel(guild.exp), true)
 		.addField('Experience:', util.numberWithCommas(guild.exp), true)
