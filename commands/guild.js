830d4f2d4b19bd2d4c269221824e108c7f8c41a2
Christopher Nethercott <ccnethercott@gmail.com>
2018-10-22 17:54:18 +0100

    Guild actually works

--- commands/guild.js
+++ commands/guild.js
@@ -2,40 +2,29 @@ const Discord = require('discord.js');
 const hypixeljs = require('hypixeljs');
 const util = require('../methods');
 
-function getGuildEmbed(guild) {
-	let guildMaster = '';
-
-	for (let i = 0; i < guild.members.length; i++) {
-		if (guild.members[i] != null) {
-			console.log(`On member ${guild.members[i].uuid} which is number ${i}`);
-			if (['GUILDMASTER', 'Guild Master'].indexOf(guild.members[i].rank) >= 0) {
-				console.log(`The player ${guild.members[i].uuid} is the guild master.`);
-				hypixeljs.getPlayer.byUuid(guild.members[i].uuid, (err, player) => {
+function sendGuildEmbed(message, guild) {
+	guild.members.forEach(member => {
+		if (member != null) {
+			if (['Guild Master', 'GUILDMASTER'].indexOf(member.rank) > -1) {
+				hypixeljs.getPlayer.byUuid(member.uuid, (err, player) => {
 					if (err) console.error(err);
-					console.log(`The guild masters username is ${player.displayname}`);
-					guildMaster = player.displayname;
+					guild.master = player.displayname;
+
+					message.channel.send(new Discord.RichEmbed()
+						.setTitle(`${guild.name}`)
+						.setURL(`https://hypixel.net/guilds/${guild._id}`)
+						.setThumbnail(guild.banner ? `https://hypixel.net/data/guild_banners/100x200/${guild._id}.png` : 'https://hypixel.net/styles/hypixel-uix/hypixel/default-guild-banner.png')
+						.addField('Guild Master:', guild.master, true)
+						.addField('Members:', guild.members.length, true)
+						.addField('Level:', util.guildLevel(guild.exp), true)
+						.addField('Experience:', util.numberWithCommas(guild.exp), true)
+						.addField('Legacy Rank:', guild.legacyRanking != undefined ? util.addSuffix(util.numberWithCommas(guild.legacyRanking)) : 'Unknown')
+						.addField('Created At:', util.formatAPITime(guild.created))
+					);
 				});
 			}
 		}
-	}
-
-	//{ uuid: '365678b8c0944feba58ed3b1488d106b',
-	//   rank: 'Guild Master',
-	//   joined: 1455213421874,
-	//   questParticipation: 4 }
-
-	if (guildMaster === '' || guildMaster === undefined || guildMaster == null) guildMaster = 'You fucked up.';
-
-	return new Discord.RichEmbed()
-		.setTitle(`${guild.name}`)
-		.setURL(`https://hypixel.net/guilds/${guild._id}`)
-		.setThumbnail(guild.banner ? `https://hypixel.net/data/guild_banners/100x200/${guild._id}.png` : 'https://hypixel.net/styles/hypixel-uix/hypixel/default-guild-banner.png')
-		.addField('Guild Master:', guildMaster, true)
-		.addField('Members:', guild.members.length, true)
-		.addField('Level:', util.guildLevel(guild.exp), true)
-		.addField('Experience:', util.numberWithCommas(guild.exp), true)
-		.addField('Legacy Rank:', guild.legacyRanking != undefined ? util.addSuffix(util.numberWithCommas(guild.legacyRanking)) : 'Unknown')
-		.addField('Created At:', util.formatAPITime(guild.created));
+	});
 }
 
 module.exports = {
@@ -47,13 +36,13 @@ module.exports = {
 			hypixeljs.getGuild.byId(args[0], (err, guild) => {
 				if (err) console.error(err);
 				if (guild == null) message.reply('that guild doesn\'t exist. Perhaps you should create it?');
-				message.channel.send(getGuildEmbed(guild));
+				sendGuildEmbed(message, guild);
 			});
 		} else {
 			hypixeljs.getGuild.byName(args.join(' '), (err, guild) => {
 				if (err) console.error(err);
 				if (guild == null) return message.reply('that guild doesn\'t exist. Perhaps you should create it?');
-				message.channel.send(getGuildEmbed(guild));
+				sendGuildEmbed(message, guild);
 			});
 		}
 	},
