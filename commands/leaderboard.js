660d92280ef151ca63ec639cbf24c909fec89267
Christopher Nethercott <ccnethercott@gmail.com>
2018-10-22 22:21:08 +0100

    Create commands/leaderboards.js

---
+++ commands/leaderboard.js
@@ -0,0 +1,33 @@
+const hypixeljs = require('hypixeljs');
+const Discord = require('discord.js');
+const fs = require('fs');
+
+const gameConversion = JSON.parse(fs.readFileSync('./files/gameconversion.json').toString());
+const allGames = Object.keys(gameConversion);
+
+function captalise(string) {
+	let lowercase = string.toString().toLocaleLowerCase();
+	return lowercase.charAt(0).toLocaleLowerCase() + lowercase.slice(1);
+}
+
+function convertGameType(userGameType) {
+	return gameConversion[userGameType.toLocaleLowerCase()];
+}
+
+module.exports = {
+	name: 'leaderboard',
+	description: 'A command to see the leaderboards of all the entire games.',
+	args: true,
+	execute(message, args) {
+		if (allGames.indexOf(args[0]) > -1) {
+			let apiGame = convertGameType(args[0]);
+			let userfriendlyGame = captalise(args[0]);
+		} else {
+			message.channel.send(new Discord.RichEmbed()
+				.setTitle(`Unknown Game Type: **${args[0]}**`)
+				.setColor('#e84118')
+				.addField('Available Game Types:', allGames.join(', '))
+			);
+		}
+	},
+};
