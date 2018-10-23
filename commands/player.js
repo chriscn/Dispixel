d67637028516c6e67373f971769c5b192c0a97c0
Christopher Nethercott <ccnethercott@gmail.com>
2018-10-23 21:54:05 +0100

    This should be working

--- commands/player.js
+++ commands/player.js
@@ -2,7 +2,7 @@ const hypixeljs = require('hypixeljs');
 const Discord = require('discord.js');
 const util = require('../methods');
 
-function sendPlayerEmbed(player) {
+function getPlayerEmbed(player) {
 	return new Discord.RichEmbed()
 		.setTitle(`${player.displayname} **(Currently ${util.isOnline(player.lastLogin, player.lastLogout)})**`)
 		.setThumbnail('https://visage.surgeplay.com/face/' + player.uuid)
@@ -10,7 +10,7 @@ function sendPlayerEmbed(player) {
 		.addField('Level:', util.networkLevel(player.networkExp), true)
 		.addField('Karma:', player.karma ? util.numberWithCommas(player.karma) : 0, true)
 		.addField('Achievement Points:', player.achievementPoints ? util.numberWithCommas(player.achievementPoints) : 0, true)
-		.addField('Joined:', !player.firstJoined ? util.formatAPITime(player.firstJoined) : 'Hasn\'t Joined', true);
+		.addField('Joined:', player.firstLogin ? util.formatAPITime(player.firstLogin) : 'Hasn\'t Joined', true);
 }
 
 module.exports = {
@@ -22,12 +22,12 @@ module.exports = {
 		if (args[0].length <= 16) {
 			hypixeljs.getPlayer.byName(args[0], (err, json) => {
 				if (err) console.error(err);
-				message.channel.send(sendPlayerEmbed(json));
+				message.channel.send(getPlayerEmbed(json));
 			});
 		} else if (args[0].match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/) || args[0].match(/[0-9a-f]{32}/)) { // tests against a full uuid or a trimmed uuid
 			hypixeljs.getPlayer.byUuid(args[0], (err, json) => {
 				if (err) console.error(err);
-				message.channel.send(sendPlayerEmbed(json));
+				message.channel.send(getPlayerEmbed(json));
 			});
 		} else {
 			message.reply(`the player name provided ${args[0]} was not valid as it was more than sixteen characters and wasn't in a UUID format. We do support UUIDs!`);
---
+++ commands/uuid.js
@@ -0,0 +1,39 @@
+const mojangjs = require('mojangjs');
+const Discord = require('discord.js');
+
+function convertTrimmedUUIDToRegular(trimmedUUID) {
+	let fullUUID = trimmedUUID;
+	fullUUID = insert(fullUUID, 8, '-');
+	fullUUID = insert(fullUUID, 13, '-');
+	fullUUID = insert(fullUUID, 18, '-');
+	fullUUID = insert(fullUUID, 23, '-');
+	return fullUUID;
+}
+
+function insert(str, index, value) {
+	return str.substr(0, index) + value + str.substr(index);
+}
+
+module.exports = {
+	name: 'uuid',
+	description: 'Gets the UUID of a given player.',
+	args: true,
+	usage: 'uuid [playername]',
+	execute(message, args) {
+		if (args.length === 1) {
+			mojangjs.getUUID(args[0], (err, res) => {
+				if (err) console.log(err);
+
+				message.channel.send(new Discord.RichEmbed()
+					.setTitle(`**${res.name}**'s UUID`)
+					.setColor('#44bd32')
+					.setThumbnail('https://visage.surgeplay.com/face/' + res)
+					.addField('UUID:', convertTrimmedUUIDToRegular(res.id))
+					.addField('Trimmed UUID:', res.id)
+				);
+			});
+		} else {
+			message.reply('You must only provide a username after the command.');
+		}
+	},
+};
