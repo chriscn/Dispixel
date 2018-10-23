196e132d4e88628ed28e66099d4e2ff68c2e9a21
Christopher Nethercott <ccnethercott@gmail.com>
2018-10-23 07:55:10 +0100

    Create uuid.js

--- commands/uuid.js
+++ commands/uuid.js
@@ -5,18 +5,20 @@ module.exports = {
 	name: 'uuid',
 	description: 'Gets the UUID of a given player.',
 	args: true,
+	usage: 'uuid [playername]',
 	execute(message, args) {
 		if (args.length == 1) {
-			mojangjs.getUUID(args[0], (err, uuid) => {
+			mojangjs.getUUID(args[0], (err, res) => {
 				if (err) console.log(err);
+
 				message.channel.send(new Discord.RichEmbed()
-					.setTitle(`**${args[0]}**`)
-					.setThumbnail('https://visage.surgeplay.com/face/' + uuid)
-					.addField('UUID', uuid)
+					.setTitle(`**${res.name}**'s UUID`)
+					.setThumbnail('https://visage.surgeplay.com/face/' + res.id)
+					.addField('UUID', res.id)
 				);
 			});
 		} else {
-
+			message.reply('You must only provide a username after the command.')
 		}
 	}
 }
