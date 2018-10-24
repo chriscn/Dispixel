f21c8daccd3d30274f0eb61bda9b9983cf62f316
Christopher Nethercott <ccnethercott@gmail.com>
2018-10-24 19:40:32 +0100

    Create leaderboard.js

---
+++ commands/leaderboard.js
@@ -0,0 +1,12 @@
+const hypixeljs = require('hypixeljs');
+const Discord = require('discord.js');
+
+module.exports - {
+	name: 'leaderboard',
+	description: 'Get\'s the most up to date leaderboards.',
+	usage: '-leaderboard [gametype]',
+	args: true,
+	execute(message, args) {
+		
+	}
+}
