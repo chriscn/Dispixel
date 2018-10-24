02c9b1acac9425911f9800e6c5e77a460b7da254
Christopher Nethercott <ccnethercott@gmail.com>
2018-10-24 07:32:51 +0100

    Removed silly functions

--- commands/uuid.js
+++ commands/uuid.js
@@ -1,19 +1,6 @@
 const mojangjs = require('mojangjs');
 const Discord = require('discord.js');
 
-function convertTrimmedUUIDToRegular(trimmedUUID) {
-	let fullUUID = trimmedUUID;
-	fullUUID = insert(fullUUID, 8, '-');
-	fullUUID = insert(fullUUID, 13, '-');
-	fullUUID = insert(fullUUID, 18, '-');
-	fullUUID = insert(fullUUID, 23, '-');
-	return fullUUID;
-}
-
-function insert(str, index, value) {
-	return str.substr(0, index) + value + str.substr(index);
-}
-
 module.exports = {
 	name: 'uuid',
 	description: 'Gets the UUID of a given player.',
