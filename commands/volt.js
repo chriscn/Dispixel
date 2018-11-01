ea7bc963e8045e49ba7ff1809d472dfc58ba4e76
Christopher Nethercott <ccnethercott@gmail.com>
2018-11-01 17:29:08 +0000

    Making sure we are in the guild discord.

--- commands/volt.js
+++ commands/volt.js
@@ -5,6 +5,10 @@ module.exprots = {
 	usage: 'volt',
 	description: 'volt',
 	execute(message) {
-		message.channel.send(new Discord.RichEmbed().setTitle('Volt').setColor('#2ecc71').addField('Guild Thread', '[Click Here](https://hypixel.net/threads/guild-social-friendly-volt.627408/)'));
+		if (message.guild.id === 148197843025395712) {
+			message.channel.send(new Discord.RichEmbed().setTitle('Volt').setColor('#2ecc71').addField('Guild Thread', '[Click Here](https://hypixel.net/threads/guild-social-friendly-volt.627408/)'));
+		} else {
+			message.reply('that command can only be run within the Volt discord. [Invite Here](https://discord.me/volt)');
+		}
 	},
-}
+};
