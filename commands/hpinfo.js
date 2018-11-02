f5ad7fda0c56a60b33a0abe74e7e19437baa0430
Christopher Nethercott <ccnethercott@gmail.com>
2018-11-02 19:06:21 +0000

    it is always an int and so doesn't need parsing

--- commands/hpinfo.js
+++ commands/hpinfo.js
@@ -18,7 +18,7 @@ module.exports = {
 					.setTitle('Hypixel Information')
 					.addField('Players Online', `There are ${playersOnline} players online`)
 					.addField('Watchdog Stats', `Today Watchdog has banned ${watchdog.watchdog_rollingDaily} players, out of ${watchdog.watchdog_total} players total.`)
-					.addField('Hypixel API Requests', `${(hypixel_api_keys.length * 120) - parseInt(hypixeljs.recentRequests)}/${hypixel_api_keys.length * 120} request(s) remaining.`)
+					.addField('Hypixel API Requests', `${(hypixel_api_keys.length * 120) - hypixeljs.recentRequests}/${hypixel_api_keys.length * 120} request(s) remaining.`)
 					.setThumbnail('https://i.imgur.com/14648Pn.png')
 					.setColor('#c7a153')
 				);
