2c507113936c03fb00d8ffd98488f3f85f1f13d7
Christopher Nethercott <ccnethercott@gmail.com>
2018-11-06 18:43:22 +0000

    Is there an achievement for this?

--- commands/friends.js
+++ commands/friends.js
@@ -1,11 +1,24 @@
 const Discord = require('discord.js');
 const hypixeljs = require('hypixeljs');
+const mojangjs = require('mojangjs');
 
 module.exports = {
 	name: 'friends',
 	usage: '[username/uuid]',
 	description: 'Gets the friends of a player.',
 	execute(message, args) {
-		message.reply('Lol.');
+		if (args[0].length <= 16) {
+			mojangjs.getUUID(args[0], (err, uuid) => {
+				if (err) console.error(err);
+				hypixeljs.getFriends(uuid, (err, friends) => {
+					if (err) console.error(err);
+					let msg = '';
+					friends.forEach(friend => {
+						msg += `${friend._id}, `;
+					});
+					message.channel.send(msg);
+				});
+			});
+		}
 	},
 };
