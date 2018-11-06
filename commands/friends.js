9e4379f2b585a9d3d67475870cb9a012591bce63
Christopher Nethercott <ccnethercott@gmail.com>
2018-11-06 18:37:24 +0000

    Create base code

---
+++ commands/friends.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const hypixeljs = require('hypixeljs');
+
+module.exports = {
+	name: 'friends',
+	usage: '[username/uuid]',
+	description: 'Gets the friends of a player.',
+	execute(message, args) {
+		message.reply('Lol.');
+	},
+};
