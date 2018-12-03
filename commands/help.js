c6e5ee810ebc4600057a97a5232c5dc1fd1e5e02
Christopher Nethercott <ccnethercott@gmail.com>
2018-12-03 20:06:45 +0000

    Using help embed

--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js');
 const { prefix } = require('../config.json');
 
 module.exports = {
@@ -7,16 +8,16 @@ module.exports = {
 	usage: '[commands name]',
 	cooldown: 5,
 	execute(message, args) {
-		const data = [];
+		let helpEmbed = new Discord.RichEmbed();
+		helpEmbed.setColor('#00a8ff');
 		const { commands } = message.client;
 
 		if (!args.length) {
-			data.push('Dispixel Help:')
-			data.push('Here\'s a list of all my commands:');
-			data.push(commands.map(command => command.name).join(', '));
-			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+			helpEmbed.setTitle('Dispixel Help:');
+			helpEmbed.addField('Here\'s a list of all of the commands available:', commands.map(command => command.name).join(', '))
+			helpEmbed.addField(`You can use ${prefix}help [command] to get information about a specific command.`)
 
-			return message.author.send(data, { split: true })
+			return message.author.send(helpEmbed)
 				.then(() => {
 					if (message.channel.type === 'dm') return;
 					message.reply('I\'ve sent you a DM with all my help message and commands!');
@@ -34,14 +35,14 @@ module.exports = {
 			return message.reply('that\'s not a valid command!');
 		}
 
-		data.push(`**Name:** ${command.name}`);
+		helpEmbed.setTitle(`${command.name} Help`)
 
-		if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
-		if (command.description) data.push(`**Description:** ${command.description}`);
-		if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+		if (command.aliases) helpEmbed.addField(`**Aliases:**`, command.aliases.join(', '));
+		if (command.description) data.push(`**Description:**`, $command.description);
+		if (command.usage) data.push(`**Usage:**`, prefix+command.name+command.usage});
 
-		data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
+		helpEmbed.addField(`**Cooldown:**`, `${command.cooldown || 3} second(s)`);
 
-		message.channel.send(data, { split: true });
+		message.channel.send(helpEmbed);
 	},
 };
