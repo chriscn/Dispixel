3245e3a6b06ca1c4a3011a84f922214c7bc78bf3
Conor <conorbyrne06@gmail.com>
2018-12-03 21:30:33 +0000

    Added info command

---
+++ commands/info.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const hypixeljs = require('hypixeljs');
+const {
+	hypixel_api_keys
+} = require('../key.json');
+
+module.exports = {
+	name: 'info',
+	description: 'Tells you information about Dispixel and the Developer!',
+	usage: 'info',
+	args: false,
+	execute(message) {
+		message.channel.send(new Discord.RichEmbed()
+			.setTitle('Dispixel Information')
+			.addField('Active in' + Discord.guilds.size + 'discord servers!')
+			.addField('Created by Thorin')
+			.setThumbnail('https://github.com/ConorTheDev/Dispixel/blob/develop/Dispixel.jpg?raw=true')
+			.setColor('#c7a153')
+		);
+	},
+};
