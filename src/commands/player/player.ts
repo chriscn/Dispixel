80696dcb9d84ae9ccd78702e859d29fe6c5d7cae
Christopher Nethercott <ccnethercott@gmail.com>
2022-03-28 16:39:30 +0100

    feat: add player rich embed

--- src/commands/player/player.ts
+++ src/commands/player/player.ts
@@ -1,4 +1,4 @@
-import { GuildMember, TextChannel } from "discord.js";
+import { GuildMember, RichEmbed, TextChannel } from "discord.js";
 import { Client } from "hypixel.ts";
 import ICommand from "../../model/command/command";
 import ECommandCategory from "../../model/command/command-category";
@@ -12,9 +12,19 @@ export default class Player implements ICommand {
     public category = ECommandCategory.PLAYER;
 
     public async execute(hpclient: Client, author: GuildMember, channel: TextChannel, args: string[]): Promise<ECommandResult> {
-        const information = await hpclient.players.fetch(args[0]);
+        const player = await hpclient.players.fetch(args[0]);
 
-        channel.send(information.lastLogin || "");
+        channel.send(new RichEmbed()
+            .setTitle(
+                `${player.displayname} **(Currently online)`
+            )
+            .setThumbnail('https://visage.surgeplay.com/face/' + player.uuid)
+            .addField('Rank:', player.packageRank || "", true)
+            .addField('Level', player.networkExp, true)
+            .addField('Karma', player.karma || 0, true)
+            .addField('Achievement Points:', player.achievementPoints || 0, true)
+            .addField('Joined', player.firstLogin || "Hasn't joined", true)
+        );
 
         return ECommandResult.SUCCESS;
     }
