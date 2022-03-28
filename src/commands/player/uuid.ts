e29557bb9eff59f67229980f36ca329b684865af
Christopher Nethercott <ccnethercott@gmail.com>
2022-03-28 20:50:55 +0100

    feat: add remaining commands

---
+++ src/commands/player/uuid.ts
@@ -0,0 +1,15 @@
+import { GuildMember, TextChannel } from "discord.js";
+import { Client } from "hypixel.ts";
+import ICommand from "../../model/command/command";
+import ECommandCategory from "../../model/command/command-category";
+import ECommandResult from "../../model/command/command-result";
+
+export default class UUID implements ICommand {
+    public name = "uuid"
+    public description = "Get a player's unique identifier"
+    public syntax = "uuid <username>"
+    public category = ECommandCategory.PLAYER
+    public async execute(author: GuildMember, channel: TextChannel, args: string[], hpclient: Client): Promise<ECommandResult> {
+        return ECommandResult.SUCCESS;
+    }
+}
---
+++ src/commands/utility/ping.ts
@@ -0,0 +1,17 @@
+import { GuildMember, TextChannel } from "discord.js";
+import { Client } from "hypixel.ts";
+import ICommand from "../../model/command/command";
+import ECommandCategory from "../../model/command/command-category";
+import ECommandResult from "../../model/command/command-result";
+
+export default class Ping implements ICommand {
+    public name = "ping"
+    public description = "Pong!"
+    public syntax = "ping"
+    public category = ECommandCategory.PLAYER
+    public async execute(author: GuildMember, channel: TextChannel, args: string[], hpclient: Client): Promise<ECommandResult> {
+        await channel.sendMessage(`Pong! ${new Date().getTime() - channel.lastMessage.createdTimestamp}ms`)
+        
+        return ECommandResult.SUCCESS;
+    }
+}
