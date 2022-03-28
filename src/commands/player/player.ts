b473f394b8bf3b58c71c033fbb44352c8c7a1a82
Christopher Nethercott <ccnethercott@gmail.com>
2022-03-28 16:49:51 +0100

    fix: convert raw api values to better values

--- src/commands/player/player.ts
+++ src/commands/player/player.ts
@@ -16,16 +16,27 @@ export default class Player implements ICommand {
 
         channel.send(new RichEmbed()
             .setTitle(
-                `${player.displayname} **(Currently online)**`
+                `${player.displayname} (Currently ${(player.lastLogin > player.lastLogout) ? 'Online' : 'Offline'})`
             )
             .setThumbnail('https://visage.surgeplay.com/face/' + player.uuid)
-            .addField('Rank:', player.packageRank || "", true)
-            .addField('Level', player.networkExp, true)
+            .addField('Rank', player.packageRank || "", true)
+            .addField('Level', networkLevel(player.networkExp || 0), true)
             .addField('Karma', player.karma || 0, true)
-            .addField('Achievement Points:', player.achievementPoints || 0, true)
-            .addField('Joined', player.firstLogin || "Hasn't joined", true)
+            .addField('Achievement Points', player.achievementPoints || 0, true)
+            .addField('Joined', new Date(player.firstLogin).toISOString() || "Hasn't joined", true)
         );
 
         return ECommandResult.SUCCESS;
     }
+}
+
+function networkLevel(networkExp: number): number {
+    const REVERSE_PQ_PREFIX = -3.5;
+	const REVERSE_CONST = 12.25;
+	const GROWTH_DIVIDES_2 = 0.0008;
+	return (networkExp < 0) ? 1 : Math.floor(
+			1 +
+					REVERSE_PQ_PREFIX +
+					Math.sqrt(REVERSE_CONST + GROWTH_DIVIDES_2 * networkExp)
+		);
 }
