1beb39e42d280e2634fb1e67202cfb382046cf9a
Christopher Nethercott <ccnethercott@gmail.com>
2022-03-28 16:04:27 +0100

    feat: implemented command framework

---
+++ src/commands/utility/help.ts
@@ -0,0 +1,95 @@
+import { GuildMember, RichEmbed, TextChannel } from "discord.js";
+import CommandRegistry from "../../controllers/commands";
+import ICommand from "../../model/command/command";
+import ECommandCategory from "../../model/command/command-category";
+import ECommandResult from "../../model/command/command-result";
+
+export default class CommandHelp implements ICommand {
+    public name = "help";
+    public aliases = [];
+    public syntax = "help <command>";
+    public description = "Gets a list of commands, or gets the specific details of the specified command.";
+    public category = ECommandCategory.UTILITY;
+
+    public async execute(author: GuildMember, channel: TextChannel, args: string[]): Promise<ECommandResult> {
+        if (args.length === 0) {
+            await channel.send(this.buildListEmbed());
+        } else {
+            const command = CommandRegistry.get(args[0].toLowerCase());
+
+            if (command) {
+                await channel.send(this.buildCommandEmbed(command));
+            } else {
+                await channel.send("The requested command couldn't be found!");
+                return ECommandResult.INVALID_SYNTAX;
+            }
+        }
+
+        return ECommandResult.SUCCESS;
+    }
+
+    private buildListEmbed(): RichEmbed {
+        const embed = new RichEmbed();
+        const categoryMap = new Map<string, ICommand[]>();
+
+        const iterator = CommandRegistry.values();
+
+        for (const command of iterator) {
+            if (!categoryMap.has(command.category.toString())) {
+                categoryMap.set(command.category.toString(), [ command ]);
+            } else {
+                const innerCategory = categoryMap.get(command.category.toString()) as ICommand[];
+
+                if (!innerCategory.includes(command)) {
+                    innerCategory.push(command);
+                }
+            }
+        }
+
+        embed.setTitle("**Commands:**");
+
+        categoryMap.forEach((commands, key) => {
+            let description = "";
+
+            commands.forEach((x) => {
+                description = `${description}${x.name}: ${x.description}\n`;
+            });
+
+            embed.addField(`${key}:`, description);
+        });
+
+        embed.setColor("#00ff00");
+
+        return embed;
+    }
+
+    private buildCommandEmbed(command: ICommand): RichEmbed {
+        const embed = new RichEmbed();
+
+        embed.setTitle(`**${command.name}:**`);
+
+        if (command.description) {
+            embed.addField("Description:", command.description);
+        }
+
+        if (command.syntax) {
+            embed.addField("Syntax:", command.syntax);
+        }
+
+        if (command.aliases.length !== 0) {
+            let aliasText = "";
+
+            command.aliases.forEach((x) => aliasText = `${aliasText}- ${x}\n`);
+
+            embed.addField("Aliases:", aliasText);
+        }
+
+        if (command.category) {
+            embed.addField("Category:", command.category.toString());
+        }
+
+        embed.setColor("#00ff00");
+
+        return embed;
+    }
+}
---
+++ src/controllers/commands.ts
@@ -0,0 +1,111 @@
+import { TextChannel } from "discord.js";
+import fs from "fs";
+import path from "path";
+import client from "../index";
+import ICommand from "../model/command/command";
+import ECommandResult from "../model/command/command-result";
+
+const commandRegistry = new Map<string, ICommand>();
+
+const regex = /[\\""](.+?)[\\""]|([^ ]+)/ig;
+const prefix = "?";
+
+gatherAllFiles(path.join(__dirname, "..", "commands"), []).forEach((x) => {
+    if (x.endsWith(".map")) { return; }
+    const command = require(x);
+    registerCommand(new command.default());
+});
+
+client.on("message", async (message) => {
+    if (!(message.channel instanceof TextChannel)) {
+        return;
+    }
+
+    if (!message.content.startsWith(prefix)) {
+        return;
+    }
+
+    const messageString = message.content.substr(prefix.length);
+
+    if (messageString.length === 0) {
+        return;
+    }
+
+    const untrimmedMatches = messageString.match(regex);
+
+    if (untrimmedMatches === null) {
+        return;
+    }
+
+    const matches: string[] = [];
+
+    untrimmedMatches.forEach((x) => {
+        matches.push(x.trim().replace(/\"/g, ""));
+    });
+
+    const command = commandRegistry.get(matches[0].toLowerCase());
+
+    if (!command) {
+        return;
+    }
+
+    const textChannel = message.channel as TextChannel;
+
+    // tslint:disable-next-line:prefer-const
+    let [resultErr, result] = await hP(command.execute(
+        message.member,
+        textChannel,
+        matches.slice(1, matches.length)
+    ));
+    if (resultErr) { result = ECommandResult.INTERNAL_ERROR; }
+
+    switch (result) {
+        case ECommandResult.NOT_ENOUGH_PERMISSION:
+            textChannel.send("You don't have the required permissions to execute this command!");
+            break;
+        case ECommandResult.INVALID_SYNTAX:
+            textChannel.send(
+                `The syntax you supplied for the command was incorrect!\n` +
+                `Please do ${prefix + "help " + command.name} for the proper usage of this command.`
+            );
+            break;
+        case ECommandResult.INTERNAL_ERROR:
+            textChannel.send("There was an internal error, please contact an administrator.");
+            break;
+    }
+});
+
+function registerCommand(command: ICommand) {
+    commandRegistry.set(command.name.toLowerCase(), command);
+    console.log(`Registered the command '${command.name}'.`);
+
+    command.aliases.forEach((x) => {
+        if (!commandRegistry.has(x.toLowerCase())) {
+            commandRegistry.set(x.toLowerCase(), command);
+            console.log(`Registered the command '${command.name}' with the alias '${x}'.`);
+        }
+    });
+}
+
+function gatherAllFiles(dir: string, filelist: string[]) {
+    const files = fs.readdirSync(dir);
+    filelist = filelist || [];
+
+    files.forEach((file) => {
+        if (fs.statSync(path.join(dir, file)).isDirectory()) {
+            filelist = gatherAllFiles(path.join(dir, file), filelist);
+        } else {
+            filelist.push(path.join(dir, file));
+        }
+    });
+
+    return filelist;
+}
+
+function hP(promise: any) {
+    return promise
+        .then((res: any) => [null, res])
+        .catch((err: any) => [err]);
+}
+
+export default commandRegistry;
