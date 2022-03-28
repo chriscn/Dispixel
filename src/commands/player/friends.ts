a4c49717284f42c784225149fd09a89887083ded
Christopher Nethercott <ccnethercott@gmail.com>
2022-03-28 20:42:07 +0100

    feat: create friends command

---
+++ src/commands/player/friends.ts
@@ -0,0 +1,16 @@
+import { GuildMember, TextChannel, User } from "discord.js";
+import { Client } from "hypixel.ts";
+import axios from "axios";
+import ICommand from "../../model/command/command";
+import ECommandCategory from "../../model/command/command-category";
+import ECommandResult from "../../model/command/command-result";
+
+export default class Friends implements ICommand {
+    public name = "friends";
+    public syntax = "friends <playername>";
+    public aliases = ['f']
+    public category = ECommandCategory.PLAYER;
+    public async execute(author: GuildMember, channel: TextChannel, args: string[], hpclient: Client): Promise<ECommandResult> {
+        return ECommandResult.SUCCESS;
+    }
+}
