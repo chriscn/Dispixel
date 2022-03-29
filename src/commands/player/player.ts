078960149b90489efb286b87db91608c94c07575
Christopher Nethercott <ccnethercott@gmail.com>
2022-03-29 10:51:35 +0100

    fix: player command should now fail / parse better

--- src/commands/player/player.ts
+++ src/commands/player/player.ts
@@ -1,10 +1,10 @@
 import { GuildMember, RichEmbed, TextChannel } from "discord.js";
-import { Client } from "hypixel.ts";
+import { Client, Player } from "hypixel.ts";
 import ICommand from "../../model/command/command";
 import ECommandCategory from "../../model/command/command-category";
 import ECommandResult from "../../model/command/command-result";
 
-export default class Player implements ICommand {
+export default class PlayerCommand implements ICommand {
     public name = "player";
     public aliases = ["p"];
     public syntax = "player <username>"
@@ -12,7 +12,7 @@ export default class Player implements ICommand {
     public category = ECommandCategory.PLAYER;
 
     public async execute(author: GuildMember, channel: TextChannel, args: string[], hpclient: Client): Promise<ECommandResult> {
-        if (args.length > 1) {
+        if (args.length != 1) {
             return ECommandResult.INVALID_SYNTAX;
         }
 
@@ -20,10 +20,10 @@ export default class Player implements ICommand {
 
         channel.send(new RichEmbed()
             .setTitle(
-                `${player.displayname} (Currently ${(player.lastLogin > player.lastLogout) ? 'Online' : 'Offline'})`
+                `${player.displayname ?? args[0]} (Currently ${(player.lastLogin > player.lastLogout) ? 'Online' : 'Offline'})`
             )
             .setThumbnail('https://visage.surgeplay.com/face/' + player.uuid)
-            .addField('Rank', player.packageRank ?? "", true)
+            .addField('Rank', parseRank(player), true)
             .addField('Level', networkLevel(player.networkExp ?? 0), true)
             .addField('Karma', player.karma ?? 0, true)
             .addField('Achievement Points', player.achievementPoints ?? 0, true)
@@ -38,9 +38,31 @@ function networkLevel(networkExp: number): number {
     const REVERSE_PQ_PREFIX = -3.5;
 	const REVERSE_CONST = 12.25;
 	const GROWTH_DIVIDES_2 = 0.0008;
-	return (networkExp < 0) ? 1 : Math.floor(
-			1 +
-					REVERSE_PQ_PREFIX +
-					Math.sqrt(REVERSE_CONST + GROWTH_DIVIDES_2 * networkExp)
-		);
+	return (networkExp < 0) ? 1 : Math.floor(1 + REVERSE_PQ_PREFIX + Math.sqrt(REVERSE_CONST + GROWTH_DIVIDES_2 * networkExp));
+}
+
+function parseRank(player: Player): string {
+    let fullRank: string;
+    let baseRank = 'Default';
+    let permRank: string | undefined; 
+    let displayRank: string | undefined;
+
+    if (player.packageRank) baseRank = replaceRank(player.packageRank);
+    if (player.newPackageRank) baseRank = replaceRank(player.newPackageRank);
+    if (player.monthlyPackageRank == 'SUPERSTAR') baseRank = 'MVP++';
+    if (player.rank && player.rank != 'NORMAL') permRank = player.rank.replace('MODERATOR', 'MOD'); 
+
+    function replaceRank(toReplace: string): string {
+        return toReplace
+            .replace('VIP_PLUS', 'VIP+')
+            .replace('MVP_PLUS', 'MVP+')
+            .replace('NONE', 'Default')
+    }
+
+    fullRank = `**[${baseRank}]**`
+    if (permRank) fullRank = `**[${permRank}]** *(actually ${baseRank})*`;
+	if (displayRank) {
+		fullRank = `**[${displayRank}]** *(${permRank ?? ''} actually ${baseRank})*`;
+	}
+	return fullRank;
 }
