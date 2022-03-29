cb395b0ddf8b52121acced09ca6e91e78a169023
Christopher Nethercott <ccnethercott@gmail.com>
2022-03-29 14:09:43 +0100

    feat: add info command

--- src/commands/utility/info.ts
+++ src/commands/utility/info.ts
@@ -1,5 +1,6 @@
-import { GuildMember, TextChannel } from "discord.js";
+import { GuildMember, RichEmbed, TextChannel } from "discord.js";
 import { Client } from "hypixel.ts";
+import { version, author, description } from "../../../package.json";
 import ICommand from "../../model/command/command";
 import ECommandCategory from "../../model/command/command-category";
 import ECommandResult from "../../model/command/command-result";
@@ -9,6 +10,15 @@ export default class InfoCommand implements ICommand {
     public description: string = "Gather information about the plugin";
     public category = ECommandCategory.UTILITY;
     public async execute(author: GuildMember, channel: TextChannel, args: string[], hpclient: Client): Promise<ECommandResult> {
+        await channel.send(new RichEmbed()
+            .setTitle('Dispixel')
+            .addField('Version', version)
+            .addField('Author', author)
+            .setFooter(description)
+            .setColor('F3D360')
+            .setImage('https://raw.githubusercontent.com/chriscn/Dispixel/main/Dispixel.jpg')
+        );
+        
         return ECommandResult.SUCCESS;
     }
 }
